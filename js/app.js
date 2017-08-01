960505b99437410430a886bed410040165430da3
tahirkhaan <tahirkhan.dream@gmail.com>
2017-08-01 11:44:32 +0500

    complete remaining part

--- js/app.js
+++ js/app.js
@@ -1,53 +1,45 @@
 function populate() {
-	if(quiz.isEnded()) {
-		//showScores();
-	}
-	else {
-		//show question
-		var element = document.getElementById("question");
-		element.innerHTML = quiz.getQuestionIndex() .text;
-
-		//Show choices
-		var_choices = quiz.getQuestionIndex() .choices;
-		for (var i = 0; i< choices.length; i++) {
-
-			var element = document.getElementById("choice" + i);
-			element.innerHTML = choices[i];
-			guess ("btn" + i, choices[i]);
-		}
-
-		showProgress();
-	} 
-
+  if (quiz.isEnded()) {
+    //showScores();
+  } else {
+    //show question
+    var element = document.getElementById("question");
+    element.innerHTML = quiz.getQuestionIndex().text;
+    //Show choices
+    var_choices = quiz.getQuestionIndex().choices;
+    for (var i = 0; i < choices.length; i++) {
+      var element = document.getElementById("choice" + i);
+      element.innerHTML = choices[i];
+      guess("btn" + i, choices[i]);
+    }
+    showProgress();
+  }
 };
-
-
 
 function guess(id, guess) {
-	var button = document. getElementById(id);
-	button.onclick = function () {
-		quiz.guess(guess);
-		populate();
-	}
+  var button = document.getElementById(id);
+  button.onclick = function() {
+    quiz.guess(guess);
+    populate();
+  }
 };
 
-
-	function showScores() {
-		var gameOverHtml = "<h1>Result</h1>";
-		gameOverHtml += "<h2 id='score'>Your Scores: " + quiz.score + "</h2>";
-	}
-
-
+function showProgress() {
+  var currentQuestionNumber = quiz.questionIndex + 1;
+  var element = document.getElementById("progress");
+  element.innerHTML = gameOverHtml
 }
 
- 
+function showScores() {
+  var gameOverHtml = "<h1>Result</h1>";
+  gameOverHtml += "<h2 id='score'>Your Scores: " + quiz.score + "</h2>";
+}
+}
 var questions = [
-	new Questions("Which one is not an object oriented progrming language?", ["java", "c++", "c", "c#"], "c");
-	new Questions("HTML stands for?", ["java", "c++", "HyperText Markup language", "c#"], "HyperText Markup language");
-	new Questions("Which language is used for styling web pages ?",["java", "css", "c", "c#"], "css");
-	new Questions("what is wordpress?",["CMS", "language", "theme", "framework"], "CMS");
+  new Questions("Which one is not an object oriented progrming language?", ["java", "c++", "c", "c#"], "c");
+  new Questions("HTML stands for?", ["java", "c++", "HyperText Markup language", "c#"], "HyperText Markup language");
+  new Questions("Which language is used for styling web pages ?", ["java", "css", "c", "c#"], "css");
+  new Questions("what is wordpress?", ["CMS", "language", "theme", "framework"], "CMS");
 ];
-
 var quiz = new Quiz(questions);
-
 populate();
--- js/quiz_controller.js
+++ js/quiz_controller.js
@@ -10,10 +10,9 @@ Quiz.prototype.isEnded = function() {
   return this.questions.length >= this.questionIndex;
 }
 Quiz.prototype.guess = function(answer) {
-    this.questionIndex++;
-    if (this.getQuestionIndex().correctAnswer(answer)) {
-        this.score++;
-      }
-
-      this.questionIndex++;
-    }
+  this.questionIndex++;
+  if (this.getQuestionIndex().correctAnswer(answer)) {
+    this.score++;
+  }
+  this.questionIndex++;
+}
